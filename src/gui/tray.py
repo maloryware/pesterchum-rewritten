fdb886925550ece898a829342e68f1f366abe885
maloryware <malory@onenetbeyond.org>
2026-05-21 02:53:00 +0100

    chore: update gitignore

--- src/gui/tray.py
+++ src/gui/tray.py
@@ -1,19 +1,19 @@
 from PySide6.QtWidgets import QSystemTrayIcon
-from PySide6.QtCore import QObject, pyqtSlot as Slot
+from PySide6.QtCore import QObject, Slot
 from PySide6.QtGui import QIcon
 from util.common import LOGGER
 
 class PesterTray(QSystemTrayIcon):
 
-    def __init__(self, icon: QIcon, mainwindow: QIcon, parent: QObject) -> None:
+    def __init__(self, icon: QIcon, parent: QObject) -> None:
         super().__init__(icon, parent)
 
     @Slot(bool)
     def change_tray_icon(self, new_message: bool) -> None:
         if new_message:
-            LOGGER.warning("Changing tray icon (blinking)")
+            LOGGER.debug("Changing tray icon (blinking)")
         else:
-            LOGGER.warning("Changing tray icon (default)")
+            LOGGER.debug("Changing tray icon (default)")
 
     @Slot
     def close_main_window(self) -> None:
