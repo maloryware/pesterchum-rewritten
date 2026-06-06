f5809ae1497beeb76ff1982754012d6e5f57b3e4
maloryware <malory@onenetbeyond.org>
2026-06-06 16:54:32 +0100

    chore: further outline PesterTray

--- src/gui/tray.py
+++ src/gui/tray.py
@@ -1,20 +1,26 @@
 from PySide6.QtWidgets import QSystemTrayIcon
 from PySide6.QtCore import QObject, Slot
 from PySide6.QtGui import QIcon
+
+from data.asset.pc_icon import PesterchumIcon
 from util.common import LOGGER
 
-class PesterTray(QSystemTrayIcon):
+class PesterTray(QObject):
+
+    # TODO: missing requirements
+    #  - loaded theme
+    icon: QSystemTrayIcon
 
     def __init__(self, icon: QIcon, parent: QObject) -> None:
-        super().__init__(icon, parent)
+        super().__init__()
 
-    @Slot(bool)
-    def change_tray_icon(self, new_message: bool) -> None:
-        if new_message:
-            LOGGER.debug("Changing tray icon (blinking)")
-        else:
-            LOGGER.debug("Changing tray icon (default)")
+    # @Slot(bool)
+    # def change_tray_icon(self, new_message: bool) -> None:
+    #     if new_message:
+    #         LOGGER.debug("Changing tray icon (blinking)")
+    #     else:
+    #         LOGGER.debug("Changing tray icon (default)")
 
-    @Slot
-    def close_main_window(self) -> None:
-        self.hide()
+    # @Slot
+    # def close_main_window(self) -> None:
+    #     self.hide()
