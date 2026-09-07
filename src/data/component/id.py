e3c182d01b7d496a2112758d0db6c56f9ca9a03b
maloryware <malory@onenetbeyond.org>
2026-09-07 04:07:09 +0100

    chore: minimally expand PesterchumID documentation

--- src/data/component/id.py
+++ src/data/component/id.py
@@ -11,6 +11,11 @@ class PesterchumID:
     Constitutes a Pesterchum ID.
 
     Pesterchum IDs make up the identity of any one user within the platform.
+    IDs are only present in the following contexts:
+    - User list
+    - Friends list
+    - Chat Window
+    - Memo Window
 
     stub
     """
