5143ba2cae3a95fb59ae9e823c09d2b8c9c3bdb1
maloryware <malory@onenetbeyond.org>
2026-09-07 04:10:31 +0100

    chore: add comment indicators to PesterLog's data system methods

--- src/data/component/log.py
+++ src/data/component/log.py
@@ -6,11 +6,14 @@ class PesterLog(PesterchumData):
     def __init__(self):
         raise NotImplementedError
 
+    # ~ PesterchumData::load ~
     def load(self) -> None:
         raise NotImplementedError
 
+    # ~ PesterchumData::save ~
     def save(self) -> None:
         raise NotImplementedError
 
+    # ~ PesterchumData::get_dir ~
     def get_dir(self) -> str:
         raise NotImplementedError
